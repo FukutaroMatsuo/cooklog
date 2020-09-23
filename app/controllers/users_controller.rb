9c4e632020ed569ef70ae94f6546a93ab6bf9c0c
FukutaroMatsuo <54502796+FukutaroMatsuo@users.noreply.github.com>
2020-09-23 13:50:30 +0900

    料理一覧のview,css,ダミーデータまで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @dishes = @user.dishes.paginate(page: params[:page], per_page: 5)  # 追記
   end
   
   def new
